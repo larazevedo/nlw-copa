4ea63fbb37a8f91c9c5723b89f06d42148469f4f
Lara Azevedo <lara.azevedo@outlook.com>
2022-11-03 19:41:38 -0300

    aula 04

--- main.js
+++ main.js
@@ -8,10 +8,13 @@ function createGame(player1, hour, player2) {
   `;
 }
 
+let delay = -0.4;
+
 
 function createCard(date, day, games) {
+  delay = delay + 0.4;
   return `
-        <div class="card">
+        <div class="card" style="animation-delay: ${delay}s">
           <h2>${date}<span>${day}</span></h2>
 
           <ul>
@@ -21,13 +24,8 @@ function createCard(date, day, games) {
     `;
 }
 
-document.querySelector("#app").innerHTML = `
-    <header>
-        <img src="./assets/logo.svg" alt="Logo da NLW" />
-      </header>
-      <main id="cards">
-      ${createCard("24/11", "quinta", createGame('brazil', '16:00', 'serbia'))}
-      ${createCard("28/11", "segunda", createGame('switzerland', '13:00', 'brazil') + createGame('portugal', '16:00', 'uruguay'))}
-      ${createCard("02/12", "sexta", createGame('brazil', '16:00', 'cameroon'))}
-      </main>
-`;
+document.querySelector("#cards").innerHTML =
+      createCard("24/11", "quinta", createGame('brazil', '16:00', 'serbia')) +
+      createCard("28/11", "segunda", createGame('switzerland', '13:00', 'brazil') + createGame('portugal', '16:00', 'uruguay')) +
+      createCard("02/12", "sexta", createGame('brazil', '16:00', 'cameroon'))
+;
